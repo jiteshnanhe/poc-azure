14fb1f7ce08b453d7454d20352f704bc091ab1cc
jiteshnanhe <158561479+jiteshnanhe@users.noreply.github.com>
2024-02-05 16:56:06 +0530

    Update createUser.js

--- src/pages/createUser.js
+++ src/pages/createUser.js
@@ -23,7 +23,7 @@ function CreateUser() {
   const handleSubmit = (e) => {
     e.preventDefault();
     console.log({user});
-    axios.get('https://wsdcrud.azurewebsites.net/api/users').then(response => {
+    axios.post('https://wsdcrud.azurewebsites.net/api/users', {user}).then(response => {
       console.log(response);
     })
   }
